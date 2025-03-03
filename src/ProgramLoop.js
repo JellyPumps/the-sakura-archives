6d9e32502d1e7b45c80d6a5c167ac435e2f81901
JellyPumps <sarthakraibhartiya@gmail.com>
2025-03-03 23:29:11 +0400

    feat: main loop implemented

--- src/ProgramLoop.js
+++ src/ProgramLoop.js
@@ -6,5 +6,25 @@ export class ProgramLoop {
 
         this.update = update;
         this.render = render;
+
+        this.raf_id = null;
+        this.is_running = false;
+    }
+
+    main_loop = (timestamp) => {
+        if (!this.is_running) return;
+
+        let delta_time = timestamp - this.last_frame_time;
+        this.last_frame_time = timestamp;
+
+        this.accumalted_time += delta_time;
+
+        while (this.accumalted_time >= this.time_step) {
+            this.update(this.time_step);
+            this.accumalted_time -= this.time_step;
+        }
+
+        this.render();
+        this.raf_id = requestAnimationFrame(this.main_loop);
     }
 }
