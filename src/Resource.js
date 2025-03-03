8f7150d6eecea9cd706146fc976dc355c9eb53af
Sarthak Rai <sarthakraibhartiya@gmail.com>
2025-03-03 21:07:15 +0400

    feat: managing website resources

---
+++ src/Resource.js
@@ -0,0 +1,27 @@
+// Safety check, so that canvas doesn't cause issues.
+class Resources {
+    constructor() {
+        // Everything that needs to be downloaded
+        this.to_load = {
+            background: "/sprites/background.png",
+        };
+
+        // Store all images
+        this.images = {};
+
+        // Load images
+        Object.keys(this.to_load).forEach(key => {
+            const img = new Image()
+            img.src = this.to_load[key];
+            this.images[key] = {
+                image: img,
+                is_loaded: false 
+            }
+            img.onload = () => {
+                this.images[key].is_loaded = true;
+            }
+        })
+    }
+}
+
+export const resources = new Resources();
