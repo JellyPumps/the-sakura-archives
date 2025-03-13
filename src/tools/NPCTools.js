bcf0a50642d3868a28dabbdba10ceb2515239309
JellyPumps <sarthakraibhartiya@gmail.com>
2025-03-13 10:35:13 +0400

    feat: moved gui prompt into NPCTools

--- src/tools/NPCTools.js
+++ src/tools/NPCTools.js
@@ -1,17 +1,41 @@
 import { Vector2 } from "../Vector2";
 
+let is_prompt_displayed = false;
+let curr_prompt_type = null;
+
+const DIALOGUE = "dialogue";
+const INTERACT = "interact";
+
 export function handle_interaction(user, npcs, input) {
+    let is_near_npc = false;
+
     npcs.forEach(npc => {
         if (check_proximity(user.position, npc.position)) {
-            // TODO add gui interaction
-            console.log("Press E to interact");
+            is_near_npc = true;
+            
+            if (!is_prompt_displayed || curr_prompt_type !== INTERACT) {
+                remove_prompt(curr_prompt_type);
+                insert_prompt(INTERACT);
+                is_prompt_displayed = true;
+                curr_prompt_type = INTERACT;
+            }
 
             if (input.is_interact_pressed()) {
+                remove_prompt(INTERACT);
                 const dialogue = npc.interact();
-                console.log(dialogue);
+                insert_prompt(DIALOGUE);
+                document.getElementById('textbox').innerText = dialogue;
+                is_prompt_displayed = true;
+                curr_prompt_type = DIALOGUE;
             }
         }
     });
+
+    if (!is_near_npc && is_prompt_displayed) {
+        remove_prompt(curr_prompt_type);
+        is_prompt_displayed = false;
+        curr_prompt_type = null;
+    }
 }
 
 function check_proximity(user_pos, npc_pos, range=1) {
@@ -24,4 +48,44 @@ function distance_to(from, to) {
     const distance_2_x = to.x - from.x;
     const distance_2_y = to.y - from.y;
     return Math.sqrt(distance_2_x**2 + distance_2_y**2);
+}
+
+const dialogue_prompt = `
+    <div id="dialogue-prompt" class="dialogue-box">
+        <p id="textbox"></p>
+        <button id="close-button">Close</button>
+    </div>
+`;
+
+const interact_prompt = `
+    <div id="interaction-prompt" class="interact-box">
+        <p id="textbox">Press E to interact</p>
+    </div>
+`;
+
+export function insert_prompt(prompt) {
+    if (prompt === DIALOGUE) {
+        if (!document.getElementById('dialogue-prompt')) {
+            document.body.insertAdjacentHTML('beforeend', dialogue_prompt);
+            document.getElementById('close-button').addEventListener('click', () => {
+                remove_prompt(DIALOGUE);
+                is_prompt_displayed = false;
+                curr_prompt_type = null;
+            });
+        }
+    } else if (prompt === INTERACT) {
+        if (!document.getElementById('interaction-prompt')) {
+            document.body.insertAdjacentHTML('beforeend', interact_prompt);
+        }
+    }
+}
+
+export function remove_prompt(prompt) {
+    if (prompt === DIALOGUE) {
+        const dialogueBox = document.getElementById('dialogue-prompt');
+        if (dialogueBox) dialogueBox.remove();
+    } else if (prompt === INTERACT) {
+        const interactBox = document.getElementById('interaction-prompt');
+        if (interactBox) interactBox.remove();
+    }
 }
